f1508a36094994007ec9224eae420ae85e629a13
Tom Brandenburg <tom.brandenburg@gmx.de>
2022-12-02 16:15:34 +0100

    correct getting token

--- frontend/spotify_manager.py
+++ frontend/spotify_manager.py
@@ -473,7 +473,7 @@ print("Opening spotipy...")
 sp_oauth=SpotifyOAuth(scope=scope, open_browser=False)
 print(" OAuth URL: " + str(sp_oauth.get_authorize_url()))
 sp_token_info = sp_oauth.get_access_token()
-print(" Access token: " + str(sp_oauth.get_access_token()))
+print(" Access token: " + str(sp_token_info))
 print(" Access token expired?: " + str(sp_oauth.is_token_expired(sp_token_info)))
 
 sp = spotipy.Spotify(auth_manager=sp_oauth)
