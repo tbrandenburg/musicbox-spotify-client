5307f8e4161a7d8baae9defeaf510504be5d7923
Tom Brandenburg <tom.brandenburg@gmx.de>
2022-12-02 15:55:58 +0100

    add token info message for console debugging

--- frontend/spotify_manager.py
+++ frontend/spotify_manager.py
@@ -472,6 +472,9 @@ print("Opening spotipy...")
 
 sp_oauth=SpotifyOAuth(scope=scope)
 print(" OAuth URL: " + str(sp_oauth.get_authorize_url()))
+sp_token_info = sp_oauth.get_access_token()
+print(" Access token: " + str(sp_oauth.get_access_token()))
+print(" Access token expired?: " + str(sp_oauth.is_token_expired(sp_token_info)))
 
 sp = spotipy.Spotify(auth_manager=sp_oauth)
 
